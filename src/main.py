a53ddac2d55c0824b65db0216de63b43b720e79a
Emelianov Artem <yaz008.yaz008@yandex.ru>
2024-09-15 22:06:56 +0300

    Add jsonify prompt

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from telebot.types import Message, File
 from utils.download import download_photo
 from utils.recognize import recognize
 from utils.gpt.ask import ask_gpt
+from json import loads
 
 @bot.message_handler(commands=['start'])
 def on_start(message: Message) -> None:
@@ -12,10 +13,12 @@ def on_start(message: Message) -> None:
 def on_photo(message: Message) -> None:
     download_photo(message=message)
     raw_tasks: str = recognize()
-    tasks: str = ask_gpt(template_name='unscramble',
-                         subs={ 'TASKS': raw_tasks })
+    unscrambled_tasks: str = ask_gpt(template_name='unscramble',
+                                     subs={ 'TASKS': raw_tasks })
+    tasks: list[str] = loads(ask_gpt(template_name='jsonify',
+                                     subs={ 'TASKS': unscrambled_tasks }))
     bot.send_message(chat_id=message.from_user.id,
-                     text=tasks)
+                     text=str(tasks))
 
 if __name__ == '__main__':
     bot.infinity_polling()
